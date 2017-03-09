165d0b71a6d61ac517f8e6e9481b1d40b7bbb977
temp <temp>
2017-03-09 17:32:53 -0600

    al putas fin

--- iluminatti.js
+++ iluminatti.js
@@ -1,20 +1,23 @@
 function draw (){
 	var base = parseInt(document.getElementById("base").value);
-	var res = '∆';
+	var res='';
 	var arresp = new Array();
 	for(var i = 0; i < base; i++ ){
 		
 		var lambda = res;
 	    arresp.push(lambda);
-		res = '<li>'+res + '∆';
+		res += '∆';
 		
 		for (var j = base - 1; j > base + 1; j--) {
+
 			var epsi = lambda;
-			llambda = '<li>'+epsi - '∆';
+			llambda = epsi - '∆';
 			arresp.push( epsi);
 		}
 
 	
 	}
-	document.getElementById("board").innerHTML = arresp.join(' <br> ');
+
+	document.getElementById("board").innerHTML = arresp.join(' <center> ');
+
 }
