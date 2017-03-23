a6d54fc3a827b85aadadea0ffb8375ae36d53598
temp <temp>
2017-03-23 15:37:56 -0600

    Formulariossss

--- formularios.js
+++ formularios.js
@@ -4,25 +4,31 @@ function info(){
 	var t3 = document.getElementById("mail").value;
 	var t4 = document.getElementById("telefono").value;
 
-	alert(t1);
-
-
-	if(t1 == null  || t2 != ' ' || t3 != '' || t4 != ''){
-		alert("ta basio");
-		var arresp = new Array();
-		var i = 0;
-		arresp[i] = text;
 	
-		var text = t1 +'<br>' + t2+'<br>'+t3+'<br>'+t4;
+	var arresp = new Array();
+	var i = 0;
+	var error = "No se recibio suficiente informacion en el formulario.";
+
+	if(t1 != null  && t2 != null && t3 != null && t4 != null){
+		alert("Un campo esta vacio.");
+		document.getElementById("imprime").innerHTML = error;
 	}
 	
 	else
 	{
-		console.log("Un campo esta vacio.");
+		alert(t1);
+		alert(t2);
+		alert(t3);
+		alert(t4);
+		
+		arresp[i] = text;
+	
+		var text = t1 +'<br>' + t2+'<br>'+t3+'<br>'+t4;
+		document.getElementById("imprime").innerHTML = text;
 	}
 	
 
 	
-	document.getElementById("imprime").innerHTML = text;
+	
 
 }
