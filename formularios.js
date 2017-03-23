a9ae9536699b897f30868a2410f2583f76d5c8be
temp <temp>
2017-03-23 17:28:49 -0600

    Falta solo eliminar chido

--- formularios.js
+++ formularios.js
@@ -1,14 +1,10 @@
 var  i = 0;
-
-
 function info(){
 	var t1 = document.getElementById("nombre").value;
 	var t2 = document.getElementById("edad").value;
 	var t3 = document.getElementById("mail").value;
 	var t4 = document.getElementById("telefono").value;
-
 	
-
 	var error = '<div id = "texto" >'+'<hr>'+"No se recibio suficiente informacion en el formulario."+'</div>';
 	
 	if(t1 && t2 && t3 && t4){
@@ -17,8 +13,9 @@ function info(){
 		//alert(name);
 		
 		var text = '<div id = "texto">'+'<hr>'+t1 +'<br>' + t2+'<br>'+t3+'<br>'+t4+'<br>'+'<button id="Eliminar" onclick="elimina();" >Eliminar</button>'+'</div>';
-		document.getElementById("texto").id= name;
 		document.getElementById("imprime").innerHTML = document.getElementById("imprime").innerHTML +text ;
+		document.getElementById("texto").id= name;
+		
 	}
 	else
 	{
@@ -26,8 +23,9 @@ function info(){
 		var name = "texto" + i;
 		alert("Un campo esta vacio.");
 		//alert(name);
+       document.getElementById("imprime").innerHTML = document.getElementById("imprime").innerHTML +error ;
         document.getElementById("texto").id= name;
-		document.getElementById("imprime").innerHTML = document.getElementById("imprime").innerHTML +error ;
+		
 	}
 
 }
