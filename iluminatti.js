56fc0fbfe95aa80a8c0304fff7db4f8475beaa82
Aarón Rodríguez <aaronrdzg@gmail.com>
2017-03-25 23:52:22 -0600

    Update iluminatti.js
    JavaScript que dibuja una pirámide dada una base.

--- iluminatti.js
+++ iluminatti.js
@@ -4,12 +4,10 @@ function draw (){
 	var arresp = new Array();
 	for(var i = 0; i < base; i++ ){
 		
-		var lambda = res;
+	    var lambda = res;
 	    arresp.push(lambda);
-		res += '∆';
-		
-		for (var j = base - 1; j > base + 1; j--) {
-
+	    res += '∆';
+	    for (var j = base - 1; j > base + 1; j--) {
 			var epsi = lambda;
 			llambda = epsi - '∆';
 			arresp.push( epsi);
