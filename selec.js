65e34f7a600b64701e32c2635d040e642b1d910e
Aarón Rodríguez <aaronrdzg@gmail.com>
2017-03-26 22:47:02 -0600

    Arreglar "board"

--- selec.js
+++ selec.js
@@ -1,14 +1,40 @@
 var lang ="", niv="";
 var res ="";
+var i = 0;
 
+var lengDinamico;
 
 function idioma(){
-	document.getElementById("langSelec").style.visibility = "visible";
+	
+	i++;
+
+	var name = "langSelec" + i;
+	//alert (name);
+
 	lang = document.getElementById("idiomas").value;
+	//alert(document.getElementById("idiomas").value);
+
+	var cuadroNiveles =   '<div id="langSelec">' 
+	 					+ '<hr>'
+						+ '<p id="seleccionado"> </p>'
+						+ '<select id="niveles" onclick="nivel();"  data-placeholder="Selecciona un nivel...">' 
+							+ '<option value = "basico">Basico</option>'
+	                		+ '<option value = "intermedio">Intermedio</option>'
+	                		+ '<option value = "avanzado">Avanzado</option>'
+	               		+ '</select>' 
+	               	    + '<button id = "eliminar" style="color:red" style="font-style: bold" onclick="elimina();">X</button>'
+	               	    + '</div>' ;
+
+	               	    var caca = document.getElementById("board").innerHTML;
+	               	    alert(caca);
+
+	document.getElementById("board").innerHTML = document.getElementById("board").innerHTML + cuadroNiveles ;
 
 	document.getElementById("seleccionado").innerHTML = lang;
+	document.getElementById("langSelec").id= name;
 
-	//alert(document.getElementById("idiomas").value);
+
+	
 
 }
 function nivel(){
